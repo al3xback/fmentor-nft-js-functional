1577a9c5333e4d18da1a731605730ca47e609c4d
alex <al3xback@gmail.com>
2024-01-24 07:39:41 +0700

    fixed title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const createHeaderEl = () => {
 	const headerEl = createElement('header');
 
-	const headerTitleEl = createElement('h1', 'sr-only', 'NFT');
+	const headerTitleEl = createElement('h1', 'sr-only', 'NFT preview card component');
 
 	headerEl.appendChild(headerTitleEl);
 
